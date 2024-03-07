a20ea15ceb8d9bfe736219861ab931f425047ae1
sushihebbar <sushi.hebbar@gmail.com>
2024-03-07 21:33:59 +0530

    second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, PES1UG21CS650\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
